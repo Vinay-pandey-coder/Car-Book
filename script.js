f23afb3c18294d7143f4df350b85d59847c5f23b
vinay pandey <abhi837688@gmail.com>
2025-02-21 21:11:44 +0530

    forth day

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 // menu
 let menu = document.querySelector('.menu-icon');
-
+let navbar = document.querySelector('.navbar');
 menu.onclick = () => {
     menu.classList.toggle('move');
+    navbar.classList.toggle('open-menu')
 };
+// close menu on page scroll 
+window.onscroll = () => {
+    menu.classList.remove('move');
+    navbar.classList.remove('open-menu')
+};
+// input form date
+window.onload = () => {
+    let today = new Date().toISOString().split('T')[0];
+    document.getElementById('start-date').value = today;
+    document.getElementById('return-date').value = new Date(
+      Date.now() + 7 * 86400000
+    )
+    .toISOString()
+    .split('T')[0];
+}
