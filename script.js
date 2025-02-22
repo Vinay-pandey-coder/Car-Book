4907ed8d1d87a3a556574805581579e2949fc613
vinay pandey <abhi837688@gmail.com>
2025-02-22 07:29:05 +0530

    last day

--- script.js
+++ script.js
@@ -10,13 +10,15 @@ window.onscroll = () => {
     menu.classList.remove('move');
     navbar.classList.remove('open-menu')
 };
-// input form date
-window.onload = () => {
-    let today = new Date().toISOString().split('T')[0];
-    document.getElementById('start-date').value = today;
-    document.getElementById('return-date').value = new Date(
-      Date.now() + 7 * 86400000
-    )
-    .toISOString()
-    .split('T')[0];
-}
+// scroll reveal animation 
+const animate = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+})
+
+animate.reveal('.nav, .heading');
+animate.reveal(".home-img img", {origin: "right"});
+animate.reveal(".input-form", {origin: "bottom"});
+animate.reveal(".trend-box, .rentals-box, .team-content, .t-box, .newsletter", {interval: 100});
